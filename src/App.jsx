f151a50ad6762188b706ccf1fa4a18a06e9e003a
Nilotpaul Nandi <nilotpaul.nandi2021@uem.edu.in>
2023-06-10 00:28:49 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,106 @@
+import { useState } from "react"
+import { music } from "./music/music"
+import { useRef } from "react"
+import { useEffect } from "react"
+import { FaPause, FaPlay, FaStepBackward, FaStepForward } from "react-icons/fa"
+import styled from "styled-components"
+import "./App.css"
+
+const App = () => {
+
+  const Leftdiv = styled.div`
+     background: url(${() => music[songindex].profile});
+  `;
+
+  // warning if page refreshes
+  useEffect(() => {
+    const unloadCallback = (event) => {
+      const e = event || window.event;
+      e.preventDefault();
+      if (e) {
+        e.returnValue = ''
+      }
+      return '';
+
+    };
+  //
+
+  // cleanup fuction after refresh
+    window.addEventListener("beforeunload", unloadCallback);
+    return () => {
+      window.removeEventListener("beforeunload", unloadCallback);
+    }
+
+  }, [])
+  //
+
+  // for play and pause
+  const audioEl = useRef(null);
+  const [play, setPlay] = useState(false);
+
+  useEffect(() => {
+    if (play) {
+      audioEl.current.play()
+    } else {
+      audioEl.current.pause()
+    }
+  }, [play])
+  const playpause = () => {
+    setPlay(!play)
+  }
+
+  //
+
+  // change song index
+  const [songindex, setSongIndex] = useState(0);
+  //
+
+  // go to previous song
+  const prevsong = () => {
+    setPlay(true)
+    if (songindex - 1 < 0) {
+      setSongIndex(music.length - 1)
+    } else {
+      setSongIndex(songindex - 1)
+    }
+  }
+  //
+
+  // go to next song
+  const nextsong = () => {
+    setPlay(true)
+    if (songindex < music.length - 1) {
+      setSongIndex(songindex + 1)
+    } else {
+      setSongIndex(0)
+    }
+  }
+  //
+
+  return (
+    <>
+      <div className="container">
+      <Leftdiv id="left-div" songindex={ songindex }>
+      <h1  songindex={ songindex }>{ music[songindex].artist }</h1>
+      </Leftdiv>
+      <div className="right-div">
+      <h2>{ music[songindex].name }</h2>
+      <img src={ music[songindex].img } alt={ music[songindex].alt } />
+      <div className="time-stamp">
+      <audio src={ music[songindex].src }
+       ref={ audioEl }
+       autoPlay
+       controls
+      /></div>
+      <div className="controls">
+      <button onClick={prevsong}><FaStepBackward className="back" /></button>
+      <button onClick={playpause}>{ play ? <FaPause className="pause" /> : <FaPlay className="play" /> }</button>
+      <button onClick={nextsong}><FaStepForward className="next" /></button>      
+      </div>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/music/music.jsx
@@ -0,0 +1,46 @@
+import audio1 from "../mp3/1.mp3"
+import audio2 from "../mp3/2.mp3"
+import audio3 from "../mp3/3.mp3"
+
+export const music = [
+    // {
+    //     name: "",
+    //     img: "",
+    //     src: ""
+    // },
+    {
+        name: "Cupid ( Twin Version )",
+        artist: "FIFTY FIFTY",
+        profile: "https://lh3.googleusercontent.com/gdN-O2b9QvB65UTOKl8VqtbkVIdAaDy07xPsek_MhAiCq3jpMnBXxCBZAH1yb6QC4gJFEU_aocRv0u0y=w1920-h800-p-l90-rj",
+        profalt: "artist-image-1",
+        img: "https://lh3.googleusercontent.com/aNYwueKXyXRauPqG7yd2m7ORbu18oJfE41tjt4mk6YkKrgtTlTl_dtD-9Jncl4MSg8xVG5IjAzcoSwI=w544-h544-l90-rj",
+        alt: "song-image-1",
+        src: audio1
+    },
+    {
+        name: "Ride It",
+        artist: "Jay Sean",
+        profile: "https://lh3.googleusercontent.com/ep_BmW2EzAF60vDPOe87e-yhoefC1bqC4pRNqCzhwZk5koSQdtaA-4_KN33e89v3u3y4BdEzdf6g0A=w960-h400-p-l90-rj",
+        profalt: "artist-image-2",
+        img: "https://lh3.googleusercontent.com/mPnnCSQfTd25JRPuOWY-tik-HoBm6BW7lgO5kD5_7eu-QF87dB3KIE46-O_SQne_k7DrXbi-x48Ab4s=w544-h544-l90-rj",
+        alt: "song-image-2",
+        src: audio2
+    },
+    {
+        name: "Die For You",
+        artist: "The Weeknd",
+        profile: "https://lh3.googleusercontent.com/U-SAmNOu4TynE818gLCfKsuHZ0U5YNEtO9mrjSI9WCCKERs98LzrCal5kajBBTQNwdcisoB2Bn-pHp4=w1500-h625-p-l90-rj",
+        profalt: "artist-image-3",
+        img: "https://lh3.googleusercontent.com/ZEDoDj27yRZJTSareSNJTwPNBJjdDMfBD8EAj3S0olbRscJlAbv7ThEA32uHvyyxeOl9d50785HSvIJ7=w544-h544-l90-rj",
+        alt: "song-image-1",
+        src: audio3  
+    }
+]
+
+    const playandpause = [
+        {
+            
+        }
+    ]
+
+export default { music }
